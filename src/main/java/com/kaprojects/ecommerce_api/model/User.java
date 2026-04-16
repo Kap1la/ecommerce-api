90bf727c2cc394fa23709e3b7aa73c95516a4c73
Kap1la <56204067+Kap1la@users.noreply.github.com>
2026-04-15 20:12:53 -0700

    Updated User.java
    Added overrides to the functions from the springframework.security UserDetails interface. Added annotations to the class and fields as needed, such as for database organization.

--- src/main/java/com/kaprojects/ecommerce_api/model/User.java
+++ src/main/java/com/kaprojects/ecommerce_api/model/User.java
@@ -16,23 +16,51 @@ import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
-public class User {
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User implements UserDetails {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(unique = true, nullable = false)
     private String email; // equivalent to username
 
+    @Column(nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
 
+    @CreationTimestamp
     private LocalDateTime createdAt;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
 
-
+    @Override
     public String getUsername() {
         return this.email;
     }
 
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
 
 }
